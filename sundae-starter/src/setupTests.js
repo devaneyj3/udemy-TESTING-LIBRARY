e8b05134e2320b978a29c6e5b126e8e2573f4d1e
devaneyj3 <jordandevaney28@gmail.com>
2025-05-08 10:16:06 -0400

    set up the mock server

--- sundae-starter/src/setupTests.js
+++ sundae-starter/src/setupTests.js
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server";
+
+// Establish API mocking before all the tests
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they affect other tests
+afterEach(() => server.resetHandlers());
+
+// Clean up afer the tests are finished
+afterAll(() => server.close());
