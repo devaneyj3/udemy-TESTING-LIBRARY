52e499d54bf1cf1f6785482949da8f284c8303d8
devaneyj3 <jordandevaney28@gmail.com>
2025-05-08 10:33:49 -0400

    set up test for the Options component

---
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Options({optionType}) {
+  return (
+    <div>Options</div>
+  )
+}
---
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+
+import Options from '../Options';
+import { expect } from 'vitest';
+
+test('displays image for each scoop option from the server', () => {
+  render(<Options optionType="scoops" />)
+  
+  //find images
+  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i })
+  expect(scoopImages).toHaveLength(2)
+
+  // confirm alt text of images
+  const altText = scoopImages.map((el) => el.alt)
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
+})
