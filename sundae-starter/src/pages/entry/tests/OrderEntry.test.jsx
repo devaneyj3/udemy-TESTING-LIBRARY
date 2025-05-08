7f7216a835357fee6c8f25406a935b289f6a66b6
devaneyj3 <jordandevaney28@gmail.com>
2025-05-08 12:49:01 -0400

    test orderEntry

---
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import OrderEntry from '../OrderEntry';
+import {server} from "../../../mocks/server";
+import { http, HttpResponse } from "msw";
+
+test('handles error for scoops and topping routes', async () => {
+  server.resetHandlers(
+    http.get("http://localhost:3030/scoops", () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+    http.get("http://localhost:3030/toppings", () => {
+      return new HttpResponse(null, { status: 500 });
+    })
+  );
+
+  render(<OrderEntry />)
+  
+  const alerts = await screen.findAllByText( "An unexpected error occurred. Please try again later.")
+  expect(alerts).toHaveLength(2)
+})
